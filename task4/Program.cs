d58a981d6e5b093c9678693ecacf37c32d35e2d9
liana16 <liana_you@bk.ru>
2022-12-03 10:33:12 +0300

    Задача 4

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿string name1 = "Bob";
+string name2 = "Adot";
+string name3 = "Fill";
+string name4 = "Liz";
+string name5 = "Ann";
+
+string[] names = {"Bob", "Adot", "Fill", "Liz", "Ann"};
+Console.WriteLine(names[1]);
